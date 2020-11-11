7762351b057190b358f76cf3033bf006a0e03851
Knut Valen <knut.valen@icloud.com>
2020-11-11 12:40:50 +0100

    simplify navigation

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -27,9 +27,9 @@ class TitleFragment : Fragment() {
                 false
         )
 
-        binding.playButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
-        }
+        binding.playButton.setOnClickListener (
+                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
 
         return binding.root
     }
